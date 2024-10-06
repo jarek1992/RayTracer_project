af0bd8ec48f343fdc1880d8f310980cb63581739
Jarek Jeda <jarek.jeda@gmail.com>
2024-10-06 21:07:56 +0200

    vec3.hpp_headerFile

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 
 int main() {
 
-  // creating an image
+  // image
   int image_width = 256;
   int image_height = 256;
 
   // render
   std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
   for (int j = 0; j < image_height; j++) {
+    std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
     for (int i = 0; i < image_width; i++) {
         auto r = double(i) / (image_width - 1);
         auto g = double(j) / (image_height -1);
@@ -21,5 +22,7 @@ int main() {
         std::cout << ir << " " << ig << " " << ib << '\n';
     }
   }
+  std::clog << "\rDone.                 \n";
+  
   return 0;
 }
