1463d52e9dbc91ee3410086eb17d7b64b4fe2e76
Jarek Jeda <jarek.jeda@gmail.com>
2024-10-08 21:34:19 +0200

    ray.hpp

---
+++ ray.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "vec3.hpp"
+
+class ray {
+public:
+    //default constructor
+    ray() {}
+    //parametric constructor, creates the object with initial values
+    ray(const point3& origin, const vec3& direction) 
+        : orig(origin)
+        , dir(direction)
+    {}
+    //getters
+    const point3& origin() const { return orig; }
+    const vec3& direction() const { return dir; }
+    //return the point at radius for parameter t
+    point3 at(double t) const {
+        return orig + t * dir;
+    }
+
+private:
+    point3 orig;
+    vec3 dir;
+};
