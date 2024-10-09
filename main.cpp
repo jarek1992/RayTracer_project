90411040f3381d09a9379b767cdbe6b6f4cb31aa
Jarek Jeda <jarek.jeda@gmail.com>
2024-10-09 18:11:24 +0200

    hit_sphere_mai.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,21 @@
 
 #include <iostream>
 
+//create a sphere 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+  vec3 oc = center - r.origin();
+  auto a = dot(r.direction(), r.direction());
+  auto b = -2.0 * dot(r.direction(), oc);
+  auto c = dot(oc, oc) - radius * radius;
+  auto discriminant = b * b - 4 * a * c;
+  return (discriminant >= 0);
+}
+
 color ray_color(const ray& r) {
+  if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
+    return color(1, 0, 0); //red color
+  }
+
   vec3 unit_direction = unit_vector(r.direction());
   auto a = 0.5 * (unit_direction.y() + 1.0);
   return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -12,7 +26,7 @@ color ray_color(const ray& r) {
 
 int main() {
 
-  // image
+  //image
   auto aspect_ratio = 16.0 / 9.0;
   int image_width = 400;
   //calculate the image height , and ensure that it's at least 1
