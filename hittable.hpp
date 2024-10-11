e3e6f4b2ac8b31ab192ad55235d6b28dc70eaeaf
Jarek Jeda <jarek.jeda@gmail.com>
2024-10-11 11:59:49 +0200

    set_face_normal

--- hittable.hpp
+++ hittable.hpp
@@ -7,6 +7,13 @@ public:
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        //sets the hit record normal vector, 'outward_normal' is assumed to have unit length
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 //virtual abstract class 
 class hittable {
--- sphere.hpp
+++ sphere.hpp
@@ -32,7 +32,8 @@ public:
         //save the record
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - center) / radius;
+        vec3 outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal); 
 
         return true;
     }
