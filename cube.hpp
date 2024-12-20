78b54db01ec76ab83e12c653b190a73084266f36
Jarek <jarek.jeda@gmail.com>
2024-12-20 17:45:31 +0100

    cube

---
+++ cube.hpp
@@ -0,0 +1,73 @@
+#pragma once	
+
+#include "hittable.hpp"
+#include "vec3.hpp"
+
+//class cube
+class cube : public hittable {
+public:
+	cube(const point3& min_corner, const point3& max_corner, shared_ptr<material> mat)
+		: min_corner(min_corner)
+		, max_corner(max_corner)
+		, mat(mat)
+	{}
+
+	//cube
+	virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+		//loop "for" goes through each axis checking interesection
+		for (int i = 0; i < 3; ++i) {
+			//distances of intersections of radius with surfaces limting cube 
+			double t0 = (min_corner[i] - r.origin()[i]) / r.direction()[i];
+			double t1 = (max_corner[i] - r.origin()[i]) / r.direction()[i];
+
+			//if t0 
+			if (t0 > t1) {
+				std::swap(t0, t1);
+			}
+			ray_t.min = std::fmax(t0, ray_t.min);
+			ray_t.max = std::fmin(t1, ray_t.max);
+
+			//no interesection
+			if (ray_t.max <= ray_t.min) {
+				return false;
+			}
+		}
+
+		//update hit_record data if radius intersect the cube
+		rec.t = ray_t.min; //set distance from starting point of the radius to the place of intersection
+		rec.p = r.at(ray_t.min); //calculate interection point using r.at(t_min) function
+		rec.normal = compute_normal(rec.p); //determine normal
+		rec.mat = mat; //assign cube material
+		return true;
+	}
+
+private:
+	point3 min_corner, max_corner; //bottom-left-back and top-rigt-front cube corners
+	shared_ptr<material> mat;
+
+	//function to determine normal vector in intersection
+	vec3 compute_normal(const point3& p) const {
+		//left cube surface 
+		if (std::fabs(p.x() - min_corner.x()) < 1e-8) {
+			return vec3(-1, 0, 0);
+		}
+		//right cube surface 
+		if (std::fabs(p.x() - min_corner.x()) < 1e-8) {
+			return vec3(1, 0, 0);
+		}
+		//bottom cube surface 
+		if (std::fabs(p.y() - min_corner.y()) < 1e-8) {
+			return vec3(0, -1, 0);
+		}
+		//top cube surface 
+		if (std::fabs(p.y() - min_corner.y()) < 1e-8) {
+			return vec3(0, 1, 0);
+		}
+		//front cube surface 
+		if (std::fabs(p.z() - min_corner.z()) < 1e-8) {
+			return vec3(0, 0, -1);
+		}
+		//back cube surface
+		return vec3(0, 0, 1); 
+	}
+};
--- main.cpp
+++ main.cpp
@@ -4,13 +4,14 @@
 #include "hittable_list.hpp"
 #include "material.hpp"
 #include "sphere.hpp"
+#include "cube.hpp"
 
 int main() {
   //set up a sphere into world
   hittable_list world;
 
   //base material and plane
-  auto material_ground = make_shared<metal>(color(0.5, 0.5, 0.5), 0.3);
+  auto material_ground = make_shared<metal>(color(0.2, 0.2, 0.2), 0.3);
   world.add(make_shared<sphere>(point3(0.0, -1000.0, 0.0), 1000.0, material_ground));
 
   //glass sphere into scene
@@ -21,38 +22,68 @@ int main() {
   auto material_bubble = make_shared<dielectric>(1.0 / 1.5);
   world.add(make_shared<sphere>(point3(0.0, 1.0, 0.0), 0.9, material_bubble));
 
-  //diffuse material sphere into scene
-  auto material_diffuse = make_shared<lambertian>(color(0.5, 0.9, 0.1));
-  world.add(make_shared<sphere>(point3(4.0, 1.0, 2.0), 1.0, material_diffuse));
+  //cube 
+  auto cube_material = make_shared<lambertian>(color(0.2, 0.5, 0.5));
+  point3 min_corner(2.0, 0.0, 2.0);
+  point3 max_corner(4.0, 2.0, 4.0);
+  world.add(make_shared<cube>(min_corner, max_corner, cube_material));
 
   //metal material sphere into scene
   auto material_metal = make_shared<metal>(color(0.2, 0.2, 0.2), 0.2);
   world.add(make_shared<sphere>(point3(6.0, 1.0, -2.0), 1.0, material_metal));
 
-  //randomize location of small spheres
+  //randomize location of small spheres and cubes
   for (int a = -20; a < 20; a++) {
     for (int b = -20; b < 20; b++) { //create the grid a(-20, 20) / b(-20, 20)
-      auto choose_material = random_double(); //randomize material for each sphere in range(0, 1)
+      auto choose_material = random_double(); //randomize material for each sphere/cube in range(0, 1)
       point3 center(a + 0.9 * random_double(), 0.2, b + 0.9 * random_double()); //point3(x,(y = const),z)
 
-      if ((center - point3(5.0, 1.0, 0.0)).length() > 0.9) { //condition to create sphere
-        shared_ptr<material> sphere_material;
+      if ((center - point3(5.0, 1.0, 0.0)).length() > 0.9) { //condition to create sphere or cube
+        shared_ptr<material> object_material;
 
         if (choose_material < 0.8) {
           //diffuse material
-          auto albedo = color::random() * color::random(); //random sphere color
-          sphere_material = make_shared<lambertian>(albedo);
-          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+          auto albedo = color::random() * color::random(); //random color
+          object_material = make_shared<lambertian>(albedo);
+
+          if (random_double() < 0.5) {
+              //crete sphere
+              world.add(make_shared<sphere>(center, 0.2, object_material));
+          } else {
+              //create cube
+              point3 min_corner(center.x() - 0.2, center.y() - 0.2, center.z() - 0.2);
+              point3 max_corner(center.x() + 0.2, center.y() + 0.2, center.z() + 0.2);
+              world.add(make_shared<cube>(min_corner, max_corner, object_material));
+          }
         } else if (choose_material < 0.95) {
           //metal material
           auto albedo = color::random(0.5, 1); //range (0.5, 1) allows to select amongst brighter range
           auto fuzz = random_double(0, 0.5); //higher value more diffused reflections
-          sphere_material = make_shared<metal>(albedo, fuzz);
-          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+          object_material = make_shared<metal>(albedo, fuzz);
+
+          if (random_double() < 0.5) {
+              //crete sphere
+              world.add(make_shared<sphere>(center, 0.2, object_material));
+          }
+          else {
+              //create cube
+              point3 min_corner(center.x() - 0.2, center.y() - 0.2, center.z() - 0.2);
+              point3 max_corner(center.x() + 0.2, center.y() + 0.2, center.z() + 0.2);
+              world.add(make_shared<cube>(min_corner, max_corner, object_material));
+          }
         } else {
           //glass material
-          sphere_material = make_shared<dielectric>(1.5);
-          world.add(make_shared<sphere>(center, 0.2, sphere_material));
+          object_material = make_shared<dielectric>(1.5);
+
+          if (random_double() < 0.5) {
+              //crete sphere
+              world.add(make_shared<sphere>(center, 0.2, object_material));
+          } else {
+              //create cube
+              point3 min_corner(center.x() - 0.2, center.y() - 0.2, center.z() - 0.2);
+              point3 max_corner(center.x() + 0.2, center.y() + 0.2, center.z() + 0.2);
+              world.add(make_shared<cube>(min_corner, max_corner, object_material));
+          }
         }
       }
     }
@@ -63,8 +94,8 @@ int main() {
 
   //image aspects ratio
   cam.aspect_ratio = 16.0 / 9.0;
-  cam.image_width = 1200;
-  cam.samples_per_pixel = 500;
+  cam.image_width = 200;
+  cam.samples_per_pixel = 50;
   cam.max_depth = 50;
 
   //camera settings
